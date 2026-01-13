f69eab401bf65ae4899f2adab19369d23c83d984
DmytroPI-dev <mailto@i-dmytro.org>
2026-01-13 17:32:34 +0100

    game working

--- main.go
+++ main.go
@@ -1,31 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
-	"hangman/hangman"
-	"strings"
+	"hangman/ui"
 )
 
 func main() {
-	word, err := hangman.GetRandomWord()
-	word = strings.ToLower(word)
-	if err != nil { // Use hangman.GetRandomWord()
-		fmt.Printf("error getting word: %s, using default word! ", err)
-		word = "golang"
-	}
-	a := app.New()
-	w := a.NewWindow("Hello")
-
-	hello := widget.NewLabel("")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
-	))
-
-	w.ShowAndRun()
+	myApplication := app.New()
+	gameWindow := myApplication.NewWindow("Go Hangman")
+	drawer := ui.NewHangmanDrawer()
+	gameUI := ui.NewGameUI(gameWindow, drawer)
+	gameUI.StartNewGame()
+	gameWindow.Resize(fyne.NewSize(400, 600))
+	gameWindow.ShowAndRun()
 }
