afe543a4c1b00bf90eb4bf9d22e95b43e441ff71
DmytroPI-dev <mailto@i-dmytro.org>
2026-01-27 21:58:33 +0100

    boilerplate for frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Box, Button, Heading } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <Box p={8}>
+      <Heading>Hangman Game</Heading>
+      <Button colorScheme="blue" mt={4}>
+        Test Button
+      </Button>
+    </Box>
+  )
+}
+
+export default App
